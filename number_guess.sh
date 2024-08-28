9994e8858d7f775d6bc39a657b610845a2a2b2bb
Daniel Fořt <danielfort2005@gmail.com>
2024-08-28 14:17:33 +0000

    feat: add GUESSER function

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,8 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-RANDOM_NUMBER=$((1 + $RANDOM % 1000 ))
+SECRET_NUMBER=$((1 + $RANDOM % 1000 ))
+NUMBER_OF_GUESSES=0;
 
 WELCOME() {
   echo "Enter your username: "
@@ -14,13 +15,71 @@ WELCOME() {
   
   if [[ -z $USER ]]
   then
+    FIRST_TIME=TRUE
     echo "Welcome, $USERNAME! It looks like this is your first time here."
   else 
-    echo "Welcome back, $USER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+    FIRST_TIME=FALSE
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
 
-
-
+  GUESSER
 }
+
+GUESSER() {
+  if [[ -z $1 ]]
+  then 
+    echo "Guess the secret number between 1 and 1000:"
+    read GUESS
+    let NUMBER_OF_GUESSES+=1
+    GUESSER $GUESS
+
+  else
+    if [[ ! $1 =~ ^[0-9]+$ ]]
+    then
+      echo "That is not an integer, guess again:"
+      read GUESS
+      GUESSER $GUESS
+
+    else
+      if [[ $1 -eq $SECRET_NUMBER ]]
+      then
+        if [[ $FIRST_TIME ]]
+        then
+          DATABASE_RESULT=$($PSQL "INSERT INTO users_games( username, games_played, best_game) VALUES( '$USERNAME', 1, $NUMBER_OF_GUESSES);")
+
+        else
+          let GAMES_PLAYED+=1
+          DATABASE_RESULT=$($PSQL "UPDATE users_games SET games_played=$GAMES_PLAYED WHERE username='$USERNAME' ")
+
+          if [[ $BEST_GAME -gt $NUMBER_OF_GUESSES ]]
+          then
+            DATABASE_RESULT=$($PSQL "UPDATE users_games SET best_game=$NUMBER_OF_GUESSES WHERE username='$USERNAME' ")
+
+          fi
+
+        fi
+
+        echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+      elif [[ $1 -gt $SECRET_NUMBER ]]
+      then
+        echo "It's lower than that, guess again:"
+        read GUESS
+        let NUMBER_OF_GUESSES+=1
+        GUESSER $GUESS
+
+      else
+        echo "It's higher than that, guess again:"
+        read GUESS
+        let NUMBER_OF_GUESSES+=1
+        GUESSER $GUESS
+
+      fi
+
+    fi
+
+  fi
+}
+
 
 WELCOME
